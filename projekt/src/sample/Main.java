5bcab76074cb6549efbbb0d2a20d0d138387c1b1
Louisetranborg <louise-tranborg@hotmail.com>
2019-05-08 22:48:53 +0200

    test

--- projekt/src/sample/Main.java
+++ projekt/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("IMat");
-        primaryStage.setScene(new Scene(root, 1440, 900));
+        primaryStage.setScene(new Scene(root, 1440, 900));  //storleken på fömstret
         primaryStage.show();
     }
 
