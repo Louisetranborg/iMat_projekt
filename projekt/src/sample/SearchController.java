ebe417a2658933a5e8686e0861bff5d7e05b1628
Louisetranborg <louise-tranborg@hotmail.com>
2019-05-09 16:41:26 +0200

    Lite i headern
    Inloggad som Hjördis

--- projekt/src/sample/SearchController.java
+++ projekt/src/sample/SearchController.java
@@ -3,8 +3,11 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import se.chalmers.cse.dat216.project.Customer;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,10 +16,16 @@ public class SearchController implements Initializable {
 
     @FXML private TextField searchBox;
     @FXML private FlowPane categoryPane;
+    @FXML private FlowPane cartPane;
     @FXML private Button minSidaButton;
+    @FXML private Label loginLable;
+
+    IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        iMatDataHandler.getCustomer().setFirstName("Hjördis"); //Sätter namnet till Hjärdis sålänge. 
+        loginLable.setText("Inloggad som " + iMatDataHandler.getCustomer().getFirstName()); //hämtar användarens namn och skriver ut det i headern.
 
     }
 }
