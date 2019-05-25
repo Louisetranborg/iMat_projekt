9514163c28c0c6343b75d6b8f7b9361f239576ad
Eyzic <einar@ingemarsson.nu>
2019-05-25 13:21:00 +0200

    MyPages fungerar typ.

--- projekt/src/sample/MyPage.java
+++ projekt/src/sample/MyPage.java
@@ -2,11 +2,24 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import se.chalmers.cse.dat216.project.Order;
 
 import java.io.IOException;
 
 public class MyPage extends StackPane {
+
+    @FXML VBox settingsPane;
+    @FXML
+    AnchorPane historyPage;
+    @FXML AnchorPane favoritePage;
+    @FXML AnchorPane savedCartPage;
+    @FXML AnchorPane personalDataPage;
+    @FXML VBox historyOrderVbox;
+
     public MyPage(){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml_filer/myPage.fxml"));
         fxmlLoader.setRoot(this);
@@ -17,6 +30,11 @@ public class MyPage extends StackPane {
         } catch(IOException exception){
             throw new RuntimeException(exception);
         }
+
+        OrderItem order = new OrderItem();
+        OrderItem order2 = new OrderItem();
+        OrderItem order3 = new OrderItem();
+        historyOrderVbox.getChildren().addAll(order, order2, order3);
 
     }
 
