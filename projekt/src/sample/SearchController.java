0e5f935c1a6bfff8f71bd1de65b06c138dec4c88
Eyzic <einar@ingemarsson.nu>
2019-06-02 00:25:34 +0200

    Fix: Order skriv ut rätt i kassan.

--- projekt/src/sample/SearchController.java
+++ projekt/src/sample/SearchController.java
@@ -718,6 +718,37 @@ public class SearchController implements Initializable {
         }
     }
 
+    //Tar in en månad med 3 bokstäver och koverterar den till en int.
+    public int convertMonthStringToInt(String month){
+        switch (month.toLowerCase()){
+            case "jan":
+                return 1;
+            case "feb":
+                return 2;
+            case "mar":
+                return 3;
+            case "apr":
+                return 4;
+            case "may":
+                return 5;
+            case "jun":
+                return 6;
+            case "jul":
+                return 7;
+            case "aug":
+                return 8;
+            case "sep":
+                return 9;
+            case "oct":
+                return 10;
+            case "nov":
+                return 11;
+            case "dec":
+                return 12;
+        }
+        return 0;
+    }
+
 
     //Vår initialize-metod, typ som en kontruktor
     @Override
