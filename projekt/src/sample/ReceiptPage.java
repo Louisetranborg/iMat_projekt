ec43f69d1e75c3e9a9a267e0767732ea4dda2d0a
Eyzic <einar@ingemarsson.nu>
2019-06-02 20:03:26 +0200

    Fix: VIsa kvitto visar rätt information.

--- projekt/src/sample/ReceiptPage.java
+++ projekt/src/sample/ReceiptPage.java
@@ -5,8 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import se.chalmers.cse.dat216.project.Order;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class ReceiptPage extends AnchorPane {
 
@@ -16,7 +18,11 @@ public class ReceiptPage extends AnchorPane {
     @FXML private Label deliveryAdr;
     @FXML private Label totalPrice;
 
+    double totalAmountPrice;
+    int totalAmount;
+
     SearchController parentController;
+    private DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
     public ReceiptPage(Order order, SearchController parentController) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml_filer/receipt.fxml"));        //Laddar in rätt fxml-fil
@@ -31,14 +37,27 @@ public class ReceiptPage extends AnchorPane {
 
 
         this.parentController = parentController;
-        orderNumber.setText(String.valueOf(order.getOrderNumber()));
-        orderDate.setText(String.valueOf(order.getDate()));
-        totalPrice.setText(String.valueOf("100"));
-    }
 
-    @FXML
-    protected void onClickAddHistoryToCart(){
-        parentController.addAllHistoryItemsToCart();
+        for(ShoppingItem item : order.getItems()) {
+            System.out.println(item.toString() + " : " + item.getTotal() + " : " + item.getAmount());
+
+            totalAmountPrice += item.getTotal();
+          //  totalAmount += item.getAmount();
+        }
+        totalAmountPrice += 45;
+
+        orderNumber.setText(String.valueOf(order.getOrderNumber()));
+
+        String month = order.getDate().toString().substring(4,7);
+
+        int day = Integer.valueOf(order.getDate().toString().substring(8,10));
+        String time = order.getDate().toString().substring(11,16);
+        int year = Integer.valueOf(order.getDate().toString().substring(25,29));
+
+        //TODO Ändra så att det faktiskt fungerar för andra datum och månader.
+        orderDate.setText(day + "/" + parentController.convertMonthStringToInt(month) + " - " + year + " | " + time);
+
+        totalPrice.setText(decimalFormat.format(totalAmountPrice)+ " kr");
     }
 
     @FXML
